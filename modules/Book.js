a776c5dec21510bdf4990684b7f92f5f044bb59b
derrick <djmulinde17@gmail.com>
2022-09-27 08:36:00 +0300

    Fixed undefined error

--- modules/Book.js
+++ modules/Book.js
@@ -1,9 +1,9 @@
 export default class Books {
   constructor() {
-    this.books = undefined || [];
+    this.books = new Array();
   }
 
-  addBook = (title, author) => {
+  addBook(title, author) {
     const id = Math.floor(Math.random() * 10000);
     this.books = this.books.concat({ title, author, id });
     return this.books;
